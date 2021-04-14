ff5f277f28c7a0a242745322f02f6ec8749ec2eb
Y4SSIN <yassin_developer@outlook.com>
2021-04-14 23:55:29 +0200

    Fix given default values for subcommands and add comments

--- editor.py
+++ editor.py
@@ -23,24 +23,34 @@ def argument_parser():
     feature_subparsers = main_parser.add_subparsers(
         help='sub-command help', title='actions', dest='command')  # Dest is defined to see which subcommand is being used.
 
+    # Some arguments within the subcommand have nargs, default & const defined.
+    # nargs is for 0 or 1 argument expected.
+    # default is used when argument isn't specified.
+    # const is used when argument is specified but no value given.
+
+    # Each subparser (subcommand) has parent defined to get access to the parent's commands within that subparser (subcommand).
+
     gif_parser = feature_subparsers.add_parser(
-        'gif', parents=[parent_parser])  # Parent is defined to get access to the parent's commands within this subcommand.
-    gif_parser.add_argument('-s', '--start', default='00:00:5')
-    gif_parser.add_argument('-e', '--end', default='00:00:10')
-    gif_parser.add_argument('-m', '--measure', default='small',
-                            help='choices: small, medium, large')
+        'gif', parents=[parent_parser])
+    gif_parser.add_argument('-s', '--start', nargs='?',
+                            default='00:00:05', const='00:00:05')
+    gif_parser.add_argument('-e', '--end', nargs='?',
+                            default='00:00:10', const='00:00:10')
+    gif_parser.add_argument('-m', '--measure', nargs='?',
+                            default='small', const='small', help='choices: small, medium, large')
     gif_parser.add_argument('--sway', action='store_true')
 
     watermark_parser = feature_subparsers.add_parser(
-        'watermark', parents=[parent_parser])  # Parent is defined to get access to the parent's commands within this subcommand.
-    watermark_parser.add_argument('-p', '--position', default='bottom_right',
-                                  help='choices: top_left, top_right, bottom_left, bottom_right')
-    watermark_parser.add_argument('-m', '--measure', default='small',
-                                  help='choices: small, medium, large')
+        'watermark', parents=[parent_parser])
+    watermark_parser.add_argument('-p', '--position', nargs='?',
+                                  default='bottom_right', const='bottom_right', help='choices: top_left, top_right, bottom_left, bottom_right')
+    watermark_parser.add_argument('-m', '--measure', nargs='?',
+                                  default='small', const='small', help='choices: small, medium, large')
 
     cut_parser = feature_subparsers.add_parser(
-        'cut', parents=[parent_parser])  # Parent is defined to get access to the parent's commands within this subcommand.
-    cut_parser.add_argument('-p', '--parts', type=int, default=2)
+        'cut', parents=[parent_parser])
+    cut_parser.add_argument('-p', '--parts', type=int, nargs='?',
+                            default=2, const=2)
 
     audio_parser = feature_subparsers.add_parser(
         'audio', parents=[parent_parser])
@@ -48,7 +58,8 @@ def argument_parser():
 
     snapshot_parser = feature_subparsers.add_parser(
         'snapshot', parents=[parent_parser])
-    snapshot_parser.add_argument('--interval', type=int, default=1)
+    snapshot_parser.add_argument('--interval', type=int, nargs='?',
+                                 default=1, const=1)
 
     args = main_parser.parse_args()
     args_dict = vars(args)
