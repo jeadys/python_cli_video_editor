86822c021127f2798b1f6d4e677d189d02a250b3
yassinao <yassin.freelancer@gmail.com>
2021-04-20 23:45:41 +0200

    Refactor to dictionary check for gif and watermark size

--- features/watermark.py
+++ features/watermark.py
@@ -4,17 +4,16 @@ from moviepy.editor import VideoFileClip, CompositeVideoClip, ImageClip
 
 
 def create_watermark(f_input, f_output, v_position, h_position, f_measure, f_fps, f_overwrite):
-    if f_measure == 'small':
-        resize = 30
-    elif f_measure == 'medium':
-        resize = 60
-    elif f_measure == 'large':
-        resize = 90
+    resize = {
+        'small': 30,
+        'medium': 60,
+        'large': 90
+    }
 
     video = (VideoFileClip(str(f_input)))
 
     watermark = (ImageClip('assets/watermark.png')).set_duration(video.duration).resize(
-        height=resize).margin(right=8, top=8, left=8, bottom=8, opacity=0).set_pos((h_position, v_position))
+        height=resize[f_measure]).margin(right=8, top=8, left=8, bottom=8, opacity=0).set_pos((h_position, v_position))
 
     new_filename = f'watermark-{str(f_input.name)}'
     final_output = f_output.joinpath(new_filename)
