49720c25299737623389661702e4773be3421458
yassinao <yassin.freelancer@gmail.com>
2021-04-21 13:36:48 +0200

    Multiprocessing for adding watermark to singular and bulk videofile input

--- validations/validator.py
+++ validations/validator.py
@@ -6,7 +6,7 @@ from validations.colors import Color
 from validations.convert import convert_to_seconds, convert_to_hms
 from moviepy.editor import VideoFileClip
 from features.audio.export import export_audio
-from features.watermark import create_watermark
+from features.watermark import Watermark
 from features.snapshot import create_snapshot
 
 
@@ -24,63 +24,59 @@ class Validate:
         self.f_input = Path(value['input'])
         self.f_output = Path(value['output'])
         self.f_overwrite = value['overwrite']
-        self.video = VideoFileClip(str(self.f_input))
-        self.video_duration = round(self.video.duration)
-        self.f_fps = value['fps'] if value['fps'] else self.video.fps
+        # self.video = VideoFileClip(str(self.f_input))
+        # self.video_duration = round(self.video.duration)
+        self.bulk = value['bulk']
+        self.f_fps = value['fps']
+        self.files = None
         if self.command == 'gif' or self.command == 'watermark':
             self.f_measure = value['measure'].lower()
         if self.command == 'gif':
             self.f_starttime = convert_to_seconds(value['start'])
             self.f_endtime = convert_to_seconds(value['end'])
             self.sway = value['sway']
-        elif self.command == 'watermark':
+        if self.command == 'watermark':
             self.v_position, self.h_position = value['position'].lower().split(
                 '_')
-        elif self.command == 'cut':
+        if self.command == 'cut':
             self.f_parts = value['parts']
-        elif self.command == 'snapshot':
+        if self.command == 'snapshot':
             self.f_interval = value['interval']
 
     def check_path(self):
         Path(self.f_output).mkdir(parents=True, exist_ok=True)
 
-        if self.f_input.is_file() and self.f_output.is_dir():
+        if (self.f_input.is_dir() and self.bulk or self.f_input.is_file() and not self.bulk) and self.f_output.is_dir():
             print(cleandoc(f'''
             {Color.OKGREEN}valid input/output{Color.ENDC}
             '''))
 
-            return self.check_extension()
+            return self.retrieve_files()
         else:
             print(cleandoc(f'''
             {Color.WARNING}invalid input/output{Color.ENDC}
             '''))
 
-    def check_extension(self):
-        video_extensions = ['.mp4', '.mov', '.mkv']
-        photo_extensions = ['.jpeg', '.jpg', '.png']
-
+    def retrieve_files(self):
         print(cleandoc(f'''
         {Color.HEADER}EXTENSION CHECK{Color.ENDC}
         extension   (type)       {self.f_input.suffix}
         '''))
 
-        if self.f_input.suffix in video_extensions:
-            if self.command == 'gif' or self.command == 'watermark':
-                return self.check_measurement()
-            elif self.command == 'cut':
-                return create_cut(self.f_input, self.f_output, self.f_parts, self.video_duration, self.f_fps, self.f_overwrite)
-            elif self.command == 'audio':
-                return export_audio(self.video, self.f_input, self.f_output, self.f_overwrite)
-            elif self.command == 'snapshot':
-                return create_snapshot(self.video, self.f_input, self.f_output, self.video_duration, self.f_interval, self.f_overwrite)
-            else:
-                pass  # DO SOMETHING
-        elif self.f_input.suffix in photo_extensions:
-            pass  # DO SOMETHING
-        else:
-            print(cleandoc(f'''
-            {Color.WARNING}EXTENSION {self.f_input.suffix} INVALID{Color.ENDC}
-            '''))
+        video_extensions = ['mp4', '.mp4', 'mov', '.mov', 'mkv', '.mkv']
+
+        self.files = [self.f_input] if not self.bulk else [file for file in Path(self.f_input).glob(
+            '*') if file.suffix in video_extensions]
+
+        if self.command == 'gif' or self.command == 'watermark':
+            return self.check_measurement()
+        elif self.command == 'cut':
+            return create_cut(self.f_input, self.f_output, self.f_parts, self.video_duration, self.f_fps, self.f_overwrite)
+        elif self.command == 'audio':
+            return export_audio(self.video, self.f_input, self.f_output, self.f_overwrite)
+        elif self.command == 'snapshot':
+            return create_snapshot(self.video, self.f_input, self.f_output, self.video_duration, self.f_interval, self.f_overwrite)
+        return False
 
     def check_measurement(self):
         measurements = ['small', 'medium', 'large']
@@ -140,7 +136,7 @@ class Validate:
         '''))
 
         if self.v_position in v_pos and self.h_position in h_pos:
-            return create_watermark(self.f_input, self.f_output, self.v_position, self.h_position, self.f_measure, self.f_fps, self.f_overwrite)
+            return Watermark(self.files, self.f_output, self.v_position, self.h_position, self.f_measure, self.f_fps, self.f_overwrite).watermark_processor()
         else:
             print(cleandoc(f'''
             {Color.WARNING}POSITION {self.v_position} {self.h_position} INVALID{Color.ENDC}
